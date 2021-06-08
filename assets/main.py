82faa548508b8a79bc9b6fc258af1fe6e31f7292
Lint Action <lint-action@samuelmeuli.com>
2021-06-08 00:45:10 +0000

    Fix code style issues with Black

--- assets/main.py
+++ assets/main.py
@@ -3,9 +3,10 @@ from urllib.request import urlretrieve
 import time
 import shutil
 import sys
+
 category = sys.argv[1]
 url = "https://api.waifu.pics/many/sfw/" + category
-print('url: ', url)
+print("url: ", url)
 json = {"exclude": []}
 
 
@@ -15,5 +16,5 @@ for i in data["files"]:
     fn = i.split("https://i.waifu.pics/")[1]
     response = requests.get(i, stream=True)
     time.sleep(0.6)
-    with open("D:/coding/bot-rewrite/assets/" + category + "/" + fn, 'wb') as out_file:
+    with open("D:/coding/bot-rewrite/assets/" + category + "/" + fn, "wb") as out_file:
         shutil.copyfileobj(response.raw, out_file)
