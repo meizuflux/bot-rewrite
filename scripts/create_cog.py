3d2ec08aa48c942c295617a1d283041c26563d9f
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-06-15 10:03:04 -0400

    pycharm doesn't like my formatting?

--- scripts/create_cog.py
+++ scripts/create_cog.py
@@ -1,25 +1,21 @@
 import sys
 
 cog = sys.argv[1]
-file = sys.argv[2]
+file = cog.lower()
 
 script = f"""
 from discord.ext import commands
-import discord
 
-from utils import codeblock
-
-import core
 from core.bot import CustomBot
-from core.context import CustomContext
-
 
 __all__ = ("setup",)
+
 
 class {cog}(commands.Cog):
     def __init__(self, bot: CustomBot):
         self.bot = bot
-    
+
+
 def setup(bot: CustomBot):
     bot.add_cog({cog}(bot))
 """
