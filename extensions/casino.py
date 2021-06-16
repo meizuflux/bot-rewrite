8da62d192257668afb3e2f43429bfa351f544099
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-06-16 11:18:28 -0400

    I am very dumb

---
+++ extensions/casino.py
@@ -0,0 +1,261 @@
+import random
+
+import discord
+from discord import ui
+from discord.ext import commands
+
+import core
+from core.bot import CustomBot
+from core.context import CustomContext
+
+__all__ = ("setup",)
+
+COLORS = {
+    "win": discord.Color.green(),
+    "lose": discord.Color.red(),
+    "draw": discord.Color.gold()
+}
+DEFAULT_DESC = "`Hit`: Add another card\n`Stand`: End the game"
+suits = ("♥", "♦", "♠️", "♣")
+
+values = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11,
+}
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.suit + self.rank
+
+    def __int__(self):
+        return values[self.rank]
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in suits:
+            for rank in values:
+                self.cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        return self.cards.pop()
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == "Ace":
+            self.aces += 1
+        self.adjust_for_ace()
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+
+class Gamble:
+    def __init__(self, bet):
+        self.total = bet
+        self.value = bet
+
+    def win(self):
+        self.total += self.value
+
+    def lose(self):
+        self.total -= self.value
+
+    def blackjack(self):
+        self.total += int(self.value * 1.5)
+
+
+class Blackjack(ui.View):
+    def __init__(self, ctx: CustomContext, *, bet: int):
+        super().__init__()
+        self.ctx = ctx
+        self.embed = None
+        self.playing = True
+
+        self.deck = Deck()
+        self.deck.shuffle()
+
+        self.player = Hand()
+        self.player.add_card(self.deck.deal())
+        self.player.add_card(self.deck.deal())
+
+        self.dealer = Hand()
+        self.dealer.add_card(self.deck.deal())
+        self.dealer.add_card(self.deck.deal())
+
+        self.bet = Gamble(bet)
+
+    @ui.button(label="Hit", style=discord.ButtonStyle.success)
+    async def hit_button(self, button: ui.Button, interaction: discord.Interaction):
+        self.player.add_card(self.deck.deal())
+        await self.check(interaction)
+
+    @ui.button(label="Stand", style=discord.ButtonStyle.danger)
+    async def stand_button(self, button: ui.Button, interaction: discord.Interaction):
+        self.playing = False
+        await self.check(interaction)
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        return interaction.user == self.ctx.author
+
+    async def check(self, interaction=None):
+        player = self.player.value
+        dealer = self.dealer.value
+
+        ret = None
+        color = COLORS["win"]
+
+        emoji = "$"
+
+        if dealer > 21:
+            self.bet.win()
+            ret = f"Result: Dealer bust {emoji}{self.bet.value:,d}"
+            self.playing = False
+
+        if player == 21:
+            self.playing = False
+
+        elif player > 21:
+            self.bet.lose()
+            ret = f"Result: Player bust {emoji}-{self.bet.value:,d}"
+            color = COLORS["lose"]
+            self.playing = False
+
+        elif player > dealer and not self.playing:
+            self.bet.win()
+            ret = f"Result: Win {emoji}{self.bet.value:,d}"
+
+        elif dealer > player:
+            print(dealer)
+            print(player)
+            self.bet.lose()
+            ret = f"Result: Loss {emoji}-{self.bet.value:,d}"
+            color = COLORS["lose"]
+            self.playing = False
+
+        elif dealer == player:
+            ret = f"Result: Push, money back."
+            color = COLORS["draw"]
+            self.playing = False
+
+        else:
+            ret = DEFAULT_DESC
+
+        if interaction:
+            if self.playing is False and player <= 21:
+                while self.dealer.value < 17:
+                    self.dealer.add_card(self.deck.deal())
+                self.stop()
+            embed = self.format_embed(ret, color)
+            await interaction.response.edit_message(embed=embed, view=self)
+
+        return ret, color
+
+    def stop(self):
+        for button in self.children:
+            button.disabled = True
+        super().stop()
+
+    def format_embed(self, description, color):
+        player = self.player
+        dealer = self.dealer
+
+        some = self.playing
+
+        embed = self.ctx.bot.embed(
+            description=description,
+            color=color
+        )
+        embed.set_footer(text=f"Cards remaining: {len(self.deck.cards)}/52")
+
+        embed.add_field(
+            name="Your hand:",
+            value=(
+                f"`{'`, `'.join(str(card) for card in player.cards)}`\n"
+                f"Value: `{player.value}`"
+            )
+        )
+        dealer_cards = f"`{'`, `'.join(str(card) for card in dealer.cards)}`" if some is False else f"`?`, `{dealer.cards[1]}`"
+        dealer_value = dealer.value if some is False else int(dealer.cards[1])
+
+        embed.add_field(
+            name="Dealer's hand:",
+            value=(
+                f"{dealer_cards}\n"
+                f"Value: `{dealer_value}`"
+            )
+        )
+
+        return embed
+
+    async def start(self):
+        player = self.player.value
+        dealer = self.dealer.value
+
+        color = COLORS["win"]
+        desc = DEFAULT_DESC
+
+        if player == 21 and dealer != 21:
+            self.bet.blackjack()
+            desc = f"Result: Blackjack! {self.bet.value:,d}"
+            color = COLORS["win"]
+            self.playing = False
+            self.stop()
+
+        elif dealer == 21:
+            self.bet.lose()
+            desc = f"Result: Dealer Blackjack. -{self.bet.value:,d}"
+            color = COLORS["lose"]
+            self.playing = False
+            self.stop()
+
+        embed = self.format_embed(desc, color)
+        await self.ctx.send(embed=embed, view=self)
+
+
+class Casino(commands.Cog):
+    def __init__(self, bot: CustomBot):
+        self.bot = bot
+
+    @core.command()
+    @commands.is_owner()
+    async def blackjack(self, ctx: CustomContext):
+        game = Blackjack(ctx, bet=1000)
+        await game.start()
+
+
+def setup(bot: CustomBot):
+    bot.add_cog(Casino(bot))
---
+++ extensions/useful.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+from discord.http import Route
+
+import core
+from core.bot import CustomBot
+from core.context import CustomContext
+
+__all__ = ("setup",)
+
+from utils import checks
+
+voice_games = {
+    "youtube": '755600276941176913',
+    'poker': '755827207812677713',
+    'betrayal': '773336526917861400',
+    'fishing': '814288819477020702',
+    'chess': '832012586023256104'
+}
+
+
+class GameConverter(commands.Converter):
+    async def convert(self, ctx: CustomContext, argument: str):
+        game = voice_games.get(argument.lower().strip())
+        if game is None:
+            raise commands.BadArgument("Invalid voice game specified.")
+        return game
+
+
+class Useful(commands.Cog):
+    def __init__(self, bot: CustomBot):
+        self.bot = bot
+
+    @core.command(
+        aliases=("vcgame", "voicechatgame", "interactivegame"),
+        examples=voice_games.keys(),
+        params={
+            "game": "The game you want to play."
+        },
+        returns="An invite link for the channel."
+    )
+    @checks.can_run(create_instant_invite=True)
+    async def vc_game(self, ctx: CustomContext, game: GameConverter, channel: discord.VoiceChannel = None):
+        """Creates an invite link for an interactive game in a Voice Channel"""
+        json = {
+            'max_age': 86400,
+            'max_uses': 0,
+            'target_application_id': game,
+            'target_type': 2,
+            'temporary': False,
+            'validate': None
+        }
+        if channel is None:
+            voice_state = ctx.author.voice
+            if voice_state is None:
+                raise commands.BadArgument("You are not in a voice channel and you did not specify a channel.")
+            channel = voice_state.channel
+        data = await self.bot.http.request(Route("POST", "/channels/" + str(channel.id) + "/invites"), json=json)
+        if data.get("guild") is None or data.get("message") is not None:
+            return await ctx.send("Something went wrong when creating the invite.")
+        await ctx.send("https://discord.com/invite" + data["code"])
+
+
+def setup(bot: CustomBot):
+    bot.add_cog(Useful(bot))
