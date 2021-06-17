a6f17f7ba74a4adc458ba83d0bc625a02baa6c34
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-06-17 13:51:30 -0400

    click click click

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
+import asyncio
 import os
+from asyncio import gather
+from traceback import format_exception
 
+import click
 import discord
+from asyncpg import create_pool
 
 from core.bot import CustomBot
-from core.config import token
+from core.config import postgres_uri, token
 
-if __name__ == "__main__":
+
+def run_bot():
     intents = discord.Intents.default()
     intents.members = True
 
@@ -26,3 +32,37 @@ if __name__ == "__main__":
     os.environ["PYTHONIOENCODING"] = "UTF-8"
 
     bot.run(token)
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def main(ctx):
+    if ctx.invoked_subcommand is None:
+        run_bot()
+
+
+@main.group()
+def db():
+    pass
+
+
+@db.command()
+def init():
+    run = asyncio.get_event_loop().run_until_complete
+    try:
+        pool = run(create_pool(dsn=postgres_uri))
+    except Exception as err:
+        tb = format_exception(type(err), err.__traceback__, err)
+        click.echo(f"Could not create database connection.\n{tb}", err=True)
+        return
+
+    files = (
+        "general.sql",
+        "twitch.sql",
+        "indexes.sql",
+    )
+    coros = []
+    for file in files:
+        with open("scripts/sql/" + file, encoding="utf8") as f:
+            coros.append(pool.execute(f.read()))
+    gather(coros)
