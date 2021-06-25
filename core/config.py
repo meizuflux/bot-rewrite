d17e6a9b9cc597445e5a96240b5eda4b48d848e0
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-06-24 20:13:30 -0400

    twotter

--- core/config.py
+++ core/config.py
@@ -2,7 +2,8 @@ from typing import NamedTuple
 
 from yaml import safe_load
 
-__all__ = ("token", "prefix", "postgres_uri", "osu", "finnhub_key", "nasa_key", "perspective_key")
+__all__ = (
+"token", "prefix", "postgres_uri", "osu", "twitter_bearer_token", "finnhub_key", "nasa_key", "perspective_key")
 
 with open("config.yml") as f:
     _config = safe_load(f)
@@ -16,6 +17,7 @@ _keys = _config["keys"]
 osu = NamedTuple("Osu", [("client_id", int), ("client_secret", int)])(
     _keys["osu"]["client_id"], _keys["osu"]["client_secret"]
 )
+twitter_bearer_token = _keys["twitter_bearer_token"]
 
 finnhub_key = _keys["finnhub_key"]
 nasa_key = _keys["nasa_key"]
--- extensions/twitter.py
+++ extensions/twitter.py
@@ -1,11 +1,78 @@
+import asyncio
+
+import aiohttp
 import discord
 from discord.ext import commands
 
 import core
 from core.bot import CustomBot
+from core.config import twitter_bearer_token
 from core.context import CustomContext
 
 __all__ = ("setup",)
+
+API_URL = "https://api.twitter.com"
+
+class TwitterStreamer:
+    def __init__(self, bot: CustomBot):
+        self.bot = bot
+        self.session = None
+
+    async def start(self):
+        url = API_URL + "/2/tweets/search/stream"
+
+        # https://developer.twitter.com/en/docs/twitter-api/v1/tweets/filter-realtime/guides/connecting
+        stall_timeout = 90
+        network_error_wait = network_error_wait_step = 0.25
+        network_error_wait_max = 16
+        http_error_wait = http_error_wait_start = 5
+        http_error_wait_max = 320
+        http_420_error_wait_start = 60
+
+        if self.session is None or self.session.closed:
+            headers = self.bot.session.headers | {"Authorization": f"Bearer {twitter_bearer_token}"}
+            self.session = aiohttp.ClientSession(
+                headers=headers,
+                timeout=aiohttp.ClientTimeout(sock_read=stall_timeout)
+            )
+
+        try:
+            while True:
+                try:
+                    async with self.session.get(url) as response:
+                        if response.ok:
+                            async for line in response.content:
+                                line = line.strip()
+                                if line:
+                                    await self.on_data(line)
+                        else:
+                            if (
+                                response.status == 420
+                                and http_error_wait < http_420_error_wait_start
+                            ):
+                                http_error_wait = http_420_error_wait_start
+
+                            await asyncio.sleep(http_error_wait)
+
+                            http_error_wait *= 2
+                            if (
+                                response.status != 420
+                                and http_error_wait > http_error_wait_max
+                            ):
+                                http_error_wait = http_error_wait_max
+
+                except (aiohttp.ClientConnectionError, aiohttp.ClientPayloadError):
+                    await asyncio.sleep(network_error_wait)
+
+                    network_error_wait += network_error_wait_step
+                    network_error_wait = min(network_error_wait, network_error_wait_max)
+        except asyncio.CancelledError:
+            return
+        finally:
+            await self.session.close()
+
+    async def on_data(self, data: bytes):
+        pass
 
 
 class Twitter(commands.Cog):
